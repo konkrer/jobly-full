29355f68ad34e0eeefa764e9b46ef0897e1f40ce
konkrer <rivortex@gmail.com>
2020-11-04 21:16:15 -0800

    ready for deploy

--- src/utils/JoblyApi.js
+++ src/utils/JoblyApi.js
@@ -1,19 +1,16 @@
 import axios from 'axios';
 
+const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+
 class JoblyApi {
   static async request(endpoint, paramsOrData = {}, verb = 'get') {
-    // paramsOrData._token = // for now, hardcode token for "testing"
-    //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc' +
-    //   '3RpbmciLCJpc19hZG1pbiI6ZmFsc2UsImlhdCI6MTU1MzcwMzE1M30.' +
-    //   'COmFETEsTxN_VfIlgIKw0bYJLkvbRQNgO1XCSE8NZ0U';
-
     console.debug('API Call:', endpoint, paramsOrData, verb);
 
     try {
       return (
         await axios({
           method: verb,
-          url: `http://localhost:3001/${endpoint}`,
+          url: `${BASE_URL}/${endpoint}`,
           [verb === 'get' ? 'params' : 'data']: paramsOrData,
         })
       ).data;
